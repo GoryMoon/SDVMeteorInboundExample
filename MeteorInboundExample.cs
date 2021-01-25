9e9b125703be34c8d79d456b9420b8a7735747cd
GoryMoon <gurreja@gmail.com>
2021-01-25 03:48:52 +0100

    SimpleSoundManager Example

---
+++ MeteorInboundExample.cs
@@ -0,0 +1,19 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using static SimpleSoundManager.ModCore;
+
+namespace MeteorInboundExample
+{
+    public class MeteorInboundExample: Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.SaveLoaded += OnGameLoopOnSaveLoaded;
+        }
+
+        private void OnGameLoopOnSaveLoaded(object sender, SaveLoadedEventArgs args)
+        {
+            MusicManager.playSound("MeteorInboundContent", "MeteorInbound");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+﻿# MeteorInboundExample
+
+Example on how to use the experimental SimpleSoundManager
+
+Custom built experimental version of [SimpleSoundManager.zip](SimpleSoundManager.zip) is provided.
+
+## License
+
+Example code is license under [MIT][1]
+
+[1]: https://choosealicense.com/licenses/mit/
